cfee2e019215093c5a6042e1088112efb4693bc2
ansharma-as <anshsharma712.as@gmail.com>
2025-01-21 20:36:35 +0530

    LinkedList#8

--- LinkedList/DeleteBegin.cpp
+++ LinkedList/DeleteBegin.cpp
@@ -17,3 +17,26 @@ void printList(Node* head){
         curr=curr->next;
     }
 }
+
+Node* deleteBegin(Node* head){
+    if(head==NULL) return NULL;
+
+    else{
+        Node* temp= head-> next;
+        delete head;
+        return temp;
+    }
+    
+}
+
+int main(){
+    Node *head = new Node(10);
+    head-> next = new Node(20);
+    head-> next-> next = new Node(30);
+    head = deleteBegin(head);
+    //head =  deleteBegin(head);
+   
+    
+    printList(head);
+    return 0;
+}
