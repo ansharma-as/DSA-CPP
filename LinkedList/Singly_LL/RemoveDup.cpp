4af65ee639f27c32ba7639002291ae3ef4a8c1ea
ansharma-as <anshsharma712.as@gmail.com>
2025-01-29 00:27:19 +0530

    Linked list#18

---
+++ LinkedList/Singly_LL/RemoveDup.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+void removeDup(Node *head){
+    Node *curr=head;
+    while(curr!=NULL &&curr->next!=NULL){
+        if(curr->data==curr->next->data){
+            Node *temp=curr->next;
+            curr->next=curr->next->next;
+            delete temp;
+
+        }
+        else{
+            curr=curr->next;
+
+        }
+    }
+
+}
+
+
+void printList(Node *head){
+    Node *curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+int main(){
+    Node *head = new Node(10);
+    head ->next =new Node(20);
+    head -> next -> next = new Node(20);
+    head -> next -> next -> next =new Node(40);
+    head -> next -> next -> next -> next =new Node(40);
+    head -> next -> next -> next -> next -> next =new Node(50);
+    
+    printList(head);
+    removeDup(head);
+    cout<<endl;
+    printList(head);
+
+    return 0;
+
+}
