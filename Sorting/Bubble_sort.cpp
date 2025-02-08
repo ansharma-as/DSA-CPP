857d2256a0af1d191255c4e2f9538d1db8b0bedc
ansharma-as <anshsharma712.as@gmail.com>
2025-02-08 19:44:27 +0530

    Sorting #3

--- Sorting/Bubble_sort.cpp
+++ Sorting/Bubble_sort.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 void BubbleSort(int arr[], int n){
-    for(int i=0; i<n-1; i++){
+    for(int i=n-1; i>=0; i--){
         int didswap= 0;
-        for(int j=0; j<n-i-1; j++){
+        for(int j=0; j<i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
                 didswap = 1;
             }
         }
         if(didswap==0){
-            break;  // no more swaps needed, array is sorted
+            break;  
         }
     }
     cout<<"Sorted array: ";
