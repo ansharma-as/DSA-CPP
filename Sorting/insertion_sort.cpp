7e6de1262f52b42e722b633baf865bd8af0b43ce
ansharma-as <anshsharma712.as@gmail.com>
2025-02-10 18:34:02 +0530

    Sorting #4

--- Sorting/insertion_sort.cpp
+++ Sorting/insertion_sort.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-void insertion_sort(int arr[] , int n){
-    for(int i=0; i<=n ; i++){
+void insertion_sort(vector<int> &arr){
+    for(int i=0; i<arr.size() ; i++){
         int j=i;
         while( j>0 && arr[j-1] > arr[j] ){
             int temp= arr[j];
@@ -17,11 +17,11 @@ void insertion_sort(int arr[] , int n){
 int main(){
     int n;
     cin>>n;
-    int arr[n];
+    vector<int> arr(n);
     for(int i=0; i<n ; i++){
         cin>>arr[i];
     }
-    insertion_sort(arr,n);
+    insertion_sort(arr);
     cout<<"Sorted array: ";
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
